0fd769fd21a801176bb0df0ca87c61b8073cf5f7
Granite Bagas <granitebagas28@gmail.com>
2024-05-26 19:55:51 +0700

    feat: Add jwt to signin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,20 @@ import { AuthController } from './auth.controller';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Granitebps97!',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   providers: [AuthService, UserRepository],
   controllers: [AuthController],
 })
