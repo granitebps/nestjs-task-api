571e5feb6f1be7e2e480431eeb508042f869c515
Granite Bagas <granitebagas28@gmail.com>
2024-05-26 14:17:19 +0700

    feat: Add check on get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
@@ -31,7 +31,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException('Task not found!');
+    }
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
