cf6436c2d1f7d7290926ba3ca10aefc4457b3bcc
Granite Bagas <granitebagas28@gmail.com>
2024-05-26 13:55:00 +0700

    feat: Add api to update task status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -30,4 +30,12 @@ export class TasksService {
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    this.deleteTask(id);
+    this.tasks.push(task);
+    return task;
+  }
 }
