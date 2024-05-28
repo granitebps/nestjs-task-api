c90fa3b089390a5c8a9f9edadaf6c1988809a9e6
Granite Bagas <granitebagas28@gmail.com>
2024-05-29 00:07:30 +0700

    refactor: Move jwt secret to env and enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const PORT = 3000;
   await app.listen(PORT);
